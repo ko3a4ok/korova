b65bc8a7095c7523da536129bba2888cc9b5a51c
Roma Slyusarchuk <romaroma@google.com>
2023-02-25 16:01:50 -0800

    add env variable for java version 17

--- src/main/kotlin/io/korova/korova/KorovaApplication.kt
+++ src/main/kotlin/io/korova/korova/KorovaApplication.kt
@@ -2,7 +2,17 @@ package io.korova.korova
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping("/")
+class MainController {
+	@GetMapping("/{id}")
+	fun main(@PathVariable id: String) = "ololo $id"
+}
 @SpringBootApplication
 class KorovaApplication
 
